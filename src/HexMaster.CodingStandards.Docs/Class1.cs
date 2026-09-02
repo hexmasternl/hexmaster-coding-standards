f94facb27f9c95b2ecd347667257c8f3b1c38388
nikneem <ekeilholz+git@outlook.com>
2026-09-02 12:29:17 +0200

    Add initial project structure and implement MCP server with random number tool

---
+++ src/HexMaster.CodingStandards.Docs/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace HexMaster.CodingStandards.Docs
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/Tests/HexMaster.CodingStandards.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace HexMaster.CodingStandards.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
